bbb1dee70fc51778347ff48252308e70a58ef693
Vilhelm Bergsøe <vilhelmbergsoe@gmail.com>
2023-03-21 20:31:54 +0100

    added games feature and prismlauncher

--- home/buckbeak.nix
+++ home/buckbeak.nix
@@ -3,6 +3,7 @@
     ./global
 
     ./features/cli
+    ./features/games
     ./features/desktop/common
     ./features/desktop/spectrwm
   ];
---
+++ home/features/games/default.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ./prism-launcher.nix ];
+}
